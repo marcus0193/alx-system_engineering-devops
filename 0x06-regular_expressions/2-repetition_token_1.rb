5905a40da16e82e3d7d816335ac4f031f46722d5
marcus0193 <marwanfaisal67@gmail.com>
2024-05-07 16:05:09 -0700

    test

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -4,10 +4,10 @@ if ARGV.length != 1
   exit
 end
 test_string = ARGV[0]
-regex = /h+b+t*n+/
+regex = /h+b*t+n+/
 match_string = test_string.match(regex)
 if match_string
   match_data = match_string[0]
-  t_part = match_data.scan(/t+/).join
-  puts "hb#{t_part}n"
+  b_part = match_data.scan(/b+/).join
+  puts "h#{b_part}tn"
 end
