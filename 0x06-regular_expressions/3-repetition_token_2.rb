aa42fb75d8d271c35f7c9a0d7a8886cedcb981ca
marcus0193 <marwanfaisal67@gmail.com>
2024-05-08 15:17:50 -0700

    num3

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -4,10 +4,10 @@ if ARGV.length != 1
   exit
 end
 test_string = ARGV[0]
-regex = /h^b^t*n^/
+regex = /^hbt*n/
 match_string = test_string.match(regex)
 if match_string
   match_data = match_string[0]
   t_part = match_data.scan(/t+/).join
-  puts "hb#{t_part}n"
+  puts "hb#{t_part}n" if t_part.length > 0
 end
