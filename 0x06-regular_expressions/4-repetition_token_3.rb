042b92e260c258f780dd05fde9a923f13a3706e9
marcus0193 <marwanfaisal67@gmail.com>
2024-05-07 17:04:05 -0700

    all

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+if ARGV.length != 1
+  puts "add a string"
+  exit
+end
+test_string = ARGV[0]
+regex = /h+b+(t|o)*n+/
+match_string = test_string.match(regex)
+if match_string
+  match_data = match_string[0]
+  t_part = match_data.scan(/(t+|o)/).join
+  puts "hb#{t_part}n"
+else
+  pust "$"
+end
