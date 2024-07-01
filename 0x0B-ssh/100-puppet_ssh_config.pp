df5466cf1d95d0177357f826a46fe6282d5ca020
marcus0193 <marwanfaisal67@gmail.com>
2024-06-30 20:54:54 -0700

    all

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-file { '~/.ssh/config':
+file { '/home/root/.ssh/config':
   ensure => present,
   content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
   owner   => 'vagrant',
