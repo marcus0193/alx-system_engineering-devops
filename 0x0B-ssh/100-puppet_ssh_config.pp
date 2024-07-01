27703e236bcdb0b9fbef972b8b771a5dde97695c
marcus0193 <marwanfaisal67@gmail.com>
2024-06-30 20:36:46 -0700

    lets

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+file_line { 'Turn off passwd auth':
+  path  => '~/.ssh/config',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '~/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
+}
